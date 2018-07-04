7199d4f4a844161e8916e3696926c9217b560e73
Veijyn <38633131+Veijyn@users.noreply.github.com>
2018-07-05 01:55:42 +0200

    Add files via upload

--- A5/file_handler.c
+++ A5/file_handler.c
@@ -9,71 +9,68 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+
 void handle_arg(char *filename)
-{
-	struct stat fileinfo;
-	
-	//filename in fileinfo übergeben
+{ 
+    struct stat fileinfo;
+    
+    // Auslesen der Dateiinformationen
 	if(stat(filename, &fileinfo) == -1){
-		perror("stat");
-		_exit(EXIT_FAILURE); // ?
-	}
-	
-	//ist fileinfo Ordner?
-	if(S_ISDIR(fileinfo.st_mode)){
-		traverse_directory(filename);
-	}
-	//ist fileinfo eine normale Datei?
-	else if(S_ISREG(fileinfo.st_mode)){
-		//enthält der Dateiname die Endung .mp3?
-		if(strncmp(".mp3",&filename[(strlen(filename)-4)], 4) == 0){
-			//Gebe Pfad + Datei aus -- Aufabenteil 1
-			parse_id3v2(filename, 1);
-		}
-		else{
-			printf("%s ist keine .mp3-Datei!\n",filename);
-		}
-	}
-	else{
-		printf("Sorry Bug! %p ist kein File oder Dir\n", filename);
-	}
+        perror("stat");
+        _exit(EXIT_FAILURE);  
+    }
+    // Ist die Datei ein Ordner?   
+    if(S_ISDIR(fileinfo.st_mode)){ 
+        // Dann Aufruf von Hilfsfunktion zum Iterieren des Ordners
+        traverse_directory(filename);      
+    // Ist es eine reguläre Datei?  
+    }else if(S_ISREG(fileinfo.st_mode)){
+        // Ist die Dateiendung .mp3?
+        if(strncmp(".mp3",&filename[(strlen(filename)-4)], 4) == 0){
+                // Aufruf der Funktion um den id3v32-Tag auszulesen
+                parse_id3v2(filename, fileinfo.st_size);
+            }else{
+                printf("%s ist keine .mp3-Datei!\n",filename);
+            }
+    }else{
+        printf("Sorry Bug! %p ist kein File oder Dir\n", filename);
+    }   
 }
 
 void traverse_directory(char *dirname)
-{
-	DIR *dirinfo;
-	struct dirent *dir_read_info;
-	int bufsize; // Zufriedenheit
-	//dirname wird geöffnet und in dirinfo übergeben
+{    
+    DIR *dirinfo;
+    struct dirent *dir_read_info;
+    int bufsize; 
+    
+    // Öffnen des Verzeichnisses
 	if((dirinfo = opendir(dirname)) == NULL){
-		perror("opendir");
-		_exit(EXIT_FAILURE); // ?
-	}
-	//so lange nicht das Ende des Verzeichnisses erreicht wurde oder ein Fehler auftaucht
-	while((dir_read_info = readdir(dirinfo)) != NULL){
-		//wenn in den letzten 4 Zeichen des Dateinamens kein "." in filename gefunden wurde, schleife um eins weiter
-		if(strncmp(dir_read_info->d_name, ".", 1)==0){
-			continue; // evtl. Fehlerbehandlung
-		}
-		//für den Fall, dass "." gefunden wurde
-		//rechne aus, wie viele Zeichen der Dateipfad + Dateiname hat
-		bufsize = strlen(dirname) + strlen(dir_read_info->d_name) + 2;
-		//allokiere "buffsize"-viele Bytes
-		char *buf = malloc(bufsize);
-		//hat nicht funktioniert? ->
-		if(buf == NULL){
-			perror("malloc");
-			_exit(EXIT_FAILURE); // ?
-		}
-		snprintf(buf, bufsize, "%s/%s", dirname, dir_read_info->d_name);
-		//handle_arg mit neuem "filename" wieder aufrufen
-		handle_arg(buf);
-		//Speicher leeren
-		free(buf);
-	}
-	//Ordner schließen
-	if(closedir(dirinfo)== -1){
-		perror("closedir");
-		_exit(EXIT_FAILURE); // ?
-	}
+          perror("opendir");
+          _exit(EXIT_FAILURE);    
+    }    
+    // Verzeichnis auslesen, solange noch Dateien vorhanden sind 
+    while((dir_read_info = readdir(dirinfo)) != NULL){   
+        // Wenn der Dateiname mit einem Punkt beginnt -> Iteriere weiter      
+         if(strncmp(dir_read_info->d_name, ".", 1)==0){
+            continue;              
+         }
+         // Anlegen eines neuen Strings für den neuen Pfad
+         bufsize = strlen(dirname) + strlen(dir_read_info->d_name) + 2;
+         // Alloziiere genügend Speicher für den neuen Pfad     
+         char *buf = malloc(bufsize);
+         if(buf == NULL){
+             perror("malloc");
+             _exit(EXIT_FAILURE); 
+         }     
+         // Den neuen Pfad schreiben (zusammenfügen)   
+         snprintf(buf, bufsize, "%s/%s", dirname, dir_read_info->d_name); 
+         // Aufrufen der Ausgangsfunktion für den neuen Pfad (mit neuen Dateien)        
+         handle_arg(buf);
+         free(buf);
+    }     
+    // Schließen des Verzeichnis-Streams und Filedescriptors
+    if(closedir(dirinfo)== -1){
+           perror("closedir");
+          _exit(EXIT_FAILURE);          
+    }
 }
--- A5/mp3info.c
+++ A5/mp3info.c
@@ -2,31 +2,71 @@
 #include "file_handler.h"
 #include "mp3info.h"
 #include "utils.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 
 void parse_id3v2(char *filename, off_t filesize)
 {
-	 unsigned char v_major, v_minor;
-     unsigned int offset, length;
+     unsigned char v_major, v_minor;
+     unsigned int offset, length = 15;
      FILE *tmp_file;
      char buf[15] = {0}, isTag;
-     
+     filesize = filesize;
+     // Datei öffnen
      if((tmp_file = fopen(filename,"r")) == NULL){
            perror("fopen");
-           _exit(EXIT_FAILURE); // ?
+           _exit(EXIT_FAILURE); 
      }
+     // Header einlesen
      if(fread(buf, sizeof(*buf),14,tmp_file) < 14){
            perror("fread");
-           _exit(EXIT_FAILURE); // ?
-     }
+           _exit(EXIT_FAILURE); 
+     }    
+     // Den id3v2-Header auswerten
+     isTag = parse_header(buf, &v_major, &v_minor, &offset, &length);      
+     // Schauen, ob ein gültiger Tag vorhanden ist    
+     if(isTag == 1){        
+            // Genügend Speicher für alle Tagframes alloziieren        
+            char *tag_buf = malloc(length);                     
+            if(tag_buf == NULL){
+                  perror("malloc");
+                  _exit(EXIT_FAILURE); 
+            }        
+            unsigned int tmp_pos = 0;
+            // id3tag_t Struktur anlegen und Werte auf NULL initialisieren
+            id3tag_t ourtag;
+            ourtag.album = NULL;
+            ourtag.performer = NULL;
+            ourtag.title = NULL;
+            ourtag.year = NULL;   
+            // Den File-Positions-Indikator auf den Beginn der Tag-Frames setzen       
+            if(fseek(tmp_file, (long)offset, SEEK_SET)==-1){
+                  perror("fseek");
+                  _exit(EXIT_FAILURE);
+            }
+            // Die Tag-Frames auslesen
+            if(fread(tag_buf, sizeof(*tag_buf),length,tmp_file) < length){
+                        perror("fread");
+                        _exit(EXIT_FAILURE); 
+            } 
+            // Die einzelnen Tag-Frames auswerten und ggf. in die oben angelegte Struktur schreiben
+            while(tmp_pos < length){                
+                  tmp_pos += parse_frame(tag_buf, tmp_pos, &ourtag);                                   
+            }  
+            // Ausgabe der Daten in dem vorgesehenen Format
+            printf("%s: %s - %s (%s) - %s\n",filename, ourtag.performer, ourtag.album, ourtag.year, ourtag.title);
+            free(tag_buf);             
+     }else{
+           printf("%s: Kein gültiger id3v2-Tag!\n",filename);
+     } 
+     // Stream flushen und Schließen des Filedescriptors
      if(fclose(tmp_file) == -1){
            perror("fclose");
-           _exit(EXIT_FAILURE); // ?            
-     }       
-     length = 15;
-     isTag = parse_header(buf, &v_major, &v_minor, &offset, &length);             
-     if(isTag == 1){                
-          //fseek( 
-         
-     }         
-  
+           _exit(EXIT_FAILURE);          
+     } 
 }
